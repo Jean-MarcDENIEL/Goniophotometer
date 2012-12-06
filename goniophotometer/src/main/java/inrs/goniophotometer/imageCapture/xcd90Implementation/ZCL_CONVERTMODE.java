d5a9a196f41f3ca96e016a724fb4ca63e2f03ec9
PCPTELABO <jean-marc.deniel@inrs.fr>
2012-12-06 11:46:30 +0100

    Sony XCD capture device completed. GrayscaleImageCapture CaptureException added and exploited.

---
+++ goniophotometer/src/main/java/inrs/goniophotometer/imageCapture/xcd90Implementation/ZCL_CONVERTMODE.java
@@ -0,0 +1,21 @@
+package inrs.goniophotometer.imageCapture.xcd90Implementation;
+
+public enum ZCL_CONVERTMODE {
+	ZCL_C24bit(0),							// 24bit
+	ZCL_C16bit(1),							// 16bit
+	ZCL_C15bit(2),							// 15bit
+	ZCL_CFilter(3),							// Color Filter
+	ZCL_C32bit(4),							// 32bit
+	ZCL_CFilterRAW8G(5),					// RAW8 + G
+	ZCL_CFilterRAW16(6),					// RAW16
+	ZCL_CFilterRAW16G(7),					// RAW16 + G
+	ZCL_CFilterRAW8(3);						// RAW8  = ZCL_CFilter
+	
+	private int convertMode;
+	private ZCL_CONVERTMODE(int convert_mode){
+		convertMode = convert_mode;
+	}
+	public int getConvertMode(){
+		return convertMode;
+	}
+}
---
+++ goniophotometer/src/main/java/inrs/goniophotometer/imageCapture/xcd90Implementation/ZCL_LIBRARY.java
@@ -0,0 +1,56 @@
+package inrs.goniophotometer.imageCapture.xcd90Implementation;
+
+import com.sun.jna.Library;
+import com.sun.jna.Memory;
+import com.sun.jna.Native;
+import com.sun.jna.NativeLong;
+import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.ptr.NativeLongByReference;
+import com.sun.jna.ptr.ShortByReference;
+
+public interface ZCL_LIBRARY extends Library {
+	ZCL_LIBRARY INSTANCE = (ZCL_LIBRARY) Native.loadLibrary("ZCL", ZCL_LIBRARY.class);
+	
+	// only for V 2.10.4 version
+	//void	ZCLSetStructVersion(short Version);
+	
+	boolean ZCLGetList(ZCL_LIST List);
+	boolean ZCLCameraInfo(NativeLong hCamera, ZCL_CAMERAINFO pInfo, IntByReference pSpeed );
+	boolean ZCLOpen(long UID, NativeLongByReference hCamera);
+	boolean ZCLClose(NativeLong hCamera);
+	boolean ZCLCheckCameraMode(NativeLong hCamera, ZCL_CAMERAMODE Mode);
+	boolean ZCLSetCameraMode(NativeLong hCamera, ZCL_CAMERAMODE Mode);
+	boolean ZCLNowCameraMode(NativeLong hCamera, ZCL_CAMERAMODE Mode);
+	int 	ZCLGetLibraryRevision();
+	boolean ZCLSetFeatureValue(NativeLong hCamera, ZCL_SETFEATUREVALUE Value);
+	boolean ZCLGetImageInfo(NativeLong hCamera, ZCL_GETIMAGEINFO Info);
+	boolean ZCLIsoAlloc(NativeLong hCamera);
+	boolean ZCLIsoRelease(NativeLong  hCamera );
+	boolean ZCLIsoStart(NativeLong hCamera, short Frame);
+	boolean ZCLIsoStop(NativeLong hCamera);
+	
+	boolean ZCLImageReq(NativeLong hCamera, Memory pBuf, int Len);
+	boolean ZCLImageCompleteWait(NativeLong hCamera, Memory pBuf, IntByReference  pSpeed, ShortByReference pCycleTime, ShortByReference pCycleCount );
+	boolean ZCLImageCompleteWaitTimeOut(NativeLong hCamera, Memory pBuf, IntByReference  pSpeed, ShortByReference pCycleTime, ShortByReference pCycleCount, short Time );
+	
+	boolean ZCLVSyncReq(NativeLong hCamera, IntByReference Hd );
+	boolean ZCLVSyncCompleteWaitTimeOut(NativeLong hCamera, IntByReference Hd, int Time );
+	
+	int   	ZCLGetLastError();
+	boolean ZCLAbortImageReqAll(NativeLong hCamera );
+	boolean ZCLGetDataDepth(NativeLong hCamera, ShortByReference Depth);
+	boolean ZCLSoftTrigger(NativeLong hCamera, boolean OnOff );
+	boolean ZCLCameraInit(NativeLong   hCamera );
+	void  	ZCLCloseAllConvHandle();
+	boolean ZCLCreateConvHandle(NativeLongByReference hTbl, int Mode, int Shift, ZCL_COLORVALUE  pValue );
+	boolean ZCLReset();
+
+
+	public static final short ZCL_LIBRARY_STRUCT_VERSION = 210;
+
+
+
+
+
+	
+}
